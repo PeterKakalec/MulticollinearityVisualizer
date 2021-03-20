b0b12e16507445d46443d596fb16bdcc11a18efc
Peter Kakalec <79677962+PeterKakalec@users.noreply.github.com>
2021-03-20 13:56:20 -0400

    Add files via upload

--- MCVis.R
+++ MCVis.R
@@ -11,8 +11,8 @@ library(shiny)
 library(ggplot2)
 library(ggforce)
 
-GenParams <- function(yX1Dist,yX2Dist,degree){
-    newDat <- data.frame(yX1Dist,yX2Dist,degree)
+GenParams <- function(yX1Dist,yX2Dist,x1X2Dist){
+    newDat <- data.frame(yX1Dist,yX2Dist,x1X2Dist)
     return(newDat)
 }
 # Define UI for application that draws a histogram
@@ -36,7 +36,7 @@ ui <- fluidPage(
                         max=100,
                         value=50,
                         step=1),
-            sliderInput("degree",
+            sliderInput("x1X2Dist",
                         "Multicollinearity",
                         min=0,
                         max=100,
@@ -56,11 +56,14 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     params <- reactive ({
-        GenParams(input$yX1Dist,input$yX2Dist,input$degree)
+        GenParams(input$yX1Dist,input$yX2Dist,input$x1X2Dist)
     })
     makeCircles <- reactive({
         #converts degrees to radians for rotating X2's position
-        d2r=0.0174533
+        d2r <- 0.0174533
+    
+        #defining the radius for our circles
+        rUnit <- 1
         
         #gets %distance from inputs and converts for display
         distDat<-params()
@@ -68,26 +71,27 @@ server <- function(input, output) {
         
         x1ydist=(100-distDat$yX1Dist)
         x2ydist=(100-distDat$yX2Dist)
+        x1x2dist=(100-distDat$x1X2Dist)
         
-        #these constants seem to be based on coordinates we're using
-        x1radiusFactor=(x1ydist)/2335
-        x2radiusFactor=(x2ydist)/1655
+        #Placing X1 at a static location
+        x1x <- 15
+        x1y <- 20
+        x1 <- data.frame("x"=x1x,"y"=x1y)
         
-        #Generating values for Y position, creating a data frame
-        yx <- 15
-        yy <- 20 
-        y<-data.frame("x" = yx,"y" = yy)
+        #Placing X2 in a way which keeps it at a constant 90-degree angle from X1 (i.e. only horizontal movement)
+        x2x <- x1$x + (2*(x1x2dist/100)*rUnit)
+        x2y <- x1$y
+        x2 <- data.frame("x"=x2x,"y"=x2y)
         
-        #Generating values for X2 position, creating data frame
-        x1x <- y$x-(sqrt(x1ydist^2)/2)*x1radiusFactor
-        x1y <- y$y-(sqrt(x1ydist^2)/2)*x1radiusFactor
-        x1 <- data.frame("x" = x1x, "y" = x1y)
+        #calculating y angle relative to X2's position at 90-degrees
+        angleDat <- data.frame("x1x2"=(x1x2dist/100)*rUnit,"x1y"=(x1ydist/100)*rUnit,"x2y"=(x2ydist/100)*rUnit)
+        yAngle <- (90*d2r)-acos(((angleDat$x1y^2)+(angleDat$x1x2^2)-(angleDat$x2y^2))/(2*(angleDat$x1y*angleDat$x1x2)))
         
-        #165 is the starting degree around Y, and (60/100)*distDat$degree limits rotation from 0 overlap with X1 to perfect overlap with X1
-        x2x <- y$x + ((sqrt(x2ydist^2)/2)*sin(d2r*(165+(60/100)*distDat$degree)))*x2radiusFactor
-        x2y <- y$y + ((sqrt(x2ydist^2)/2)*cos(d2r*(165+(60/100)*distDat$degree)))*x2radiusFactor
-        x2 <- data.frame("x" = x2x, "y" = x2y)
-        #Plotting
+        #Placing Y at the correct angle and distance from X1 as to maintain its distance with X2
+        yx <- x1$x+(((2*x1ydist/100)*rUnit)*sin(yAngle))
+        yy <- x1$y+(((2*x1ydist/100)*rUnit)*cos(yAngle))
+        y <- data.frame("x" = yx, "y" = yy)
+
         df.venn <- data.frame(x = c(x1$x, y$x, x2$x),
                               y = c(x1$y, y$y, x2$y),
                               labels = c('X1', 'Y', 'X2'))
@@ -96,11 +100,11 @@ server <- function(input, output) {
     
     output$distPlot <- renderPlot({
         diagramData<-makeCircles()
-        ggplot(diagramData, aes(x0 = x, y0 = y, r = 1.5, fill = labels)) +
+        ggplot(diagramData, aes(x0 = x, y0 = y, r = 1, fill = labels)) +
             geom_circle(alpha = .3, size = 1, colour = 'grey') +
             coord_fixed() +
-            scale_x_continuous(limits=c(11,17.5))+
-            scale_y_continuous(limits=c(15,22))+
+            scale_x_continuous(limits=c(12,18))+
+            scale_y_continuous(limits=c(18.9,22))+
             theme_void()
     })
 }
